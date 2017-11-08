4ffabd63d6d4fcde7e6704a2f3424df3d5846d2e
felipe augusto silva marques <felipe_marques353@hotmail.com>
2017-11-08 15:39:45 -0200

    Correção de bug

--- APAC_TIS4/APAC_TIS4/RelatoriosDAO.cs
+++ APAC_TIS4/APAC_TIS4/RelatoriosDAO.cs
@@ -16,51 +16,52 @@ namespace APAC_TIS4
         }
 
         public Chart loadChart(Chart chartGrafico) {
-            SingletonBD singleton = SingletonBD.getInstancia();
-            using (MySqlConnection conexaoMySQL = singleton.getConexao())
-            {
-                conexaoMySQL.Open();
-                try
+            if (chartGrafico.Series.Count == 0) {
+                SingletonBD singleton = SingletonBD.getInstancia();
+                using (MySqlConnection conexaoMySQL = singleton.getConexao())
                 {
-                    MySqlCommand cmd = new MySqlCommand("SELECT * FROM vw_RelatorioLucros", conexaoMySQL);
-                    //cmd.Transaction = tran;
-                    MySqlDataReader myReader = cmd.ExecuteReader();
-
-                    while (myReader.Read())
+                    conexaoMySQL.Open();
+                    try
                     {
-                        chartGrafico.Series.Add("Lucro Janeiro");
-                        chartGrafico.Series["Lucro Janeiro"].Points.AddXY("", myReader.GetInt32("LUCRO_JAN"));
-                        chartGrafico.Series.Add("Lucro Fevereiro");
-                        chartGrafico.Series["Lucro Fevereiro"].Points.AddXY("", myReader.GetInt32("LUCRO_FEV"));
-                        chartGrafico.Series.Add("Lucro Março");
-                        chartGrafico.Series["Lucro Março"].Points.AddXY("", myReader.GetInt32("LUCRO_MAR"));
-                        chartGrafico.Series.Add("Lucro Abril");
-                        chartGrafico.Series["Lucro Abril"].Points.AddXY("", myReader.GetInt32("LUCRO_ABR"));
-                        chartGrafico.Series.Add("Lucro Maio");
-                        chartGrafico.Series["Lucro Maio"].Points.AddXY("", myReader.GetInt32("LUCRO_MAI"));
-                        chartGrafico.Series.Add("Lucro Junho");
-                        chartGrafico.Series["Lucro Junho"].Points.AddXY("", myReader.GetInt32("LUCRO_JUN"));
-                        chartGrafico.Series.Add("Lucro Julho");
-                        chartGrafico.Series["Lucro Julho"].Points.AddXY("", myReader.GetInt32("LUCRO_JUL"));
-                        chartGrafico.Series.Add("Lucro Agosto");
-                        chartGrafico.Series["Lucro Agosto"].Points.AddXY("", myReader.GetInt32("LUCRO_AGO"));
-                        chartGrafico.Series.Add("Lucro Setembro");
-                        chartGrafico.Series["Lucro Setembro"].Points.AddXY("", myReader.GetInt32("LUCRO_SET"));
-                        chartGrafico.Series.Add("Lucro Outubro");
-                        chartGrafico.Series["Lucro Outubro"].Points.AddXY("", myReader.GetInt32("LUCRO_OUT"));
-                        chartGrafico.Series.Add("Lucro Novembro");
-                        chartGrafico.Series["Lucro Novembro"].Points.AddXY("", myReader.GetInt32("LUCRO_NOV"));
-                        chartGrafico.Series.Add("Lucro Dezembro");
-                        chartGrafico.Series["Lucro Dezembro"].Points.AddXY("", myReader.GetInt32("LUCRO_DEZ"));
+                        MySqlCommand cmd = new MySqlCommand("SELECT * FROM vw_RelatorioLucros", conexaoMySQL);
+                        //cmd.Transaction = tran;
+                        MySqlDataReader myReader = cmd.ExecuteReader();
+
+                        while (myReader.Read())
+                        {
+                            chartGrafico.Series.Add("Lucro Janeiro");
+                            chartGrafico.Series["Lucro Janeiro"].Points.AddXY("", myReader.GetInt32("LUCRO_JAN"));
+                            chartGrafico.Series.Add("Lucro Fevereiro");
+                            chartGrafico.Series["Lucro Fevereiro"].Points.AddXY("", myReader.GetInt32("LUCRO_FEV"));
+                            chartGrafico.Series.Add("Lucro Março");
+                            chartGrafico.Series["Lucro Março"].Points.AddXY("", myReader.GetInt32("LUCRO_MAR"));
+                            chartGrafico.Series.Add("Lucro Abril");
+                            chartGrafico.Series["Lucro Abril"].Points.AddXY("", myReader.GetInt32("LUCRO_ABR"));
+                            chartGrafico.Series.Add("Lucro Maio");
+                            chartGrafico.Series["Lucro Maio"].Points.AddXY("", myReader.GetInt32("LUCRO_MAI"));
+                            chartGrafico.Series.Add("Lucro Junho");
+                            chartGrafico.Series["Lucro Junho"].Points.AddXY("", myReader.GetInt32("LUCRO_JUN"));
+                            chartGrafico.Series.Add("Lucro Julho");
+                            chartGrafico.Series["Lucro Julho"].Points.AddXY("", myReader.GetInt32("LUCRO_JUL"));
+                            chartGrafico.Series.Add("Lucro Agosto");
+                            chartGrafico.Series["Lucro Agosto"].Points.AddXY("", myReader.GetInt32("LUCRO_AGO"));
+                            chartGrafico.Series.Add("Lucro Setembro");
+                            chartGrafico.Series["Lucro Setembro"].Points.AddXY("", myReader.GetInt32("LUCRO_SET"));
+                            chartGrafico.Series.Add("Lucro Outubro");
+                            chartGrafico.Series["Lucro Outubro"].Points.AddXY("", myReader.GetInt32("LUCRO_OUT"));
+                            chartGrafico.Series.Add("Lucro Novembro");
+                            chartGrafico.Series["Lucro Novembro"].Points.AddXY("", myReader.GetInt32("LUCRO_NOV"));
+                            chartGrafico.Series.Add("Lucro Dezembro");
+                            chartGrafico.Series["Lucro Dezembro"].Points.AddXY("", myReader.GetInt32("LUCRO_DEZ"));
+                        }
+                    }
+                    finally
+                    {
+                        conexaoMySQL.Close();
                     }
                 }
-                finally
-                {
-                    conexaoMySQL.Close();
-                }
-
-                return chartGrafico;
             }
+            return chartGrafico;
         }
 
         public DataSet loadGrid() {
